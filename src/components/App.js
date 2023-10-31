03ebb0a1d5270c0533e2368a54fa4c90e9031667
fay <wahomeimani@gmail.com>
2023-10-31 15:03:05 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,37 @@
-// create your App component here
+// App.js
+
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [dogImage, setDogImage] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setDogImage(data.message);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error('Fetch error:', error);
+      });
+  }, []);
+
+  return (
+    <div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <img src={dogImage} alt="A Random Dog" />
+      )}
+    </div>
+  );
+}
+
+export default App;
